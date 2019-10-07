838a779ad78bacf0a526118bdc468a237532589f
Ates Yanik <albiberon@users.noreply.github.com>
2019-10-07 10:19:59 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,44 @@
+function toggleKwaliteit() {
+    var toggleButton = $("#kwaliteit-btn");
+    var element = document.getElementById("kwaliteit-arrow");
+    element.classList.toggle("rotated");
 
+    //double click prevention
+    setTimeout(function(){ 
+        $(toggleButton).prop('disabled', true);
+    }, 50);
+
+setTimeout(function(){ 
+    $(toggleButton).prop('disabled', false);
+}, 250);
+}
+
+function toggleBestellen() {
+        let bestelButton = $("#bestel-button");
+        let element = document.getElementById("bestellen-arrow");
+        element.classList.toggle("rotated");
+
+//double click prevention
+    setTimeout(function(){ 
+            $(bestelButton).prop('disabled', true);
+        }, 50);
+
+    setTimeout(function(){ 
+        $(bestelButton).prop('disabled', false);
+    }, 250);
+    }
+
+function toggleKlantenservice() {
+    let klantenserviceButton = $("#klantenservice-button")
+    let element = document.getElementById("klantenservice-arrow");
+    element.classList.toggle("rotated");
+
+    //double click prevention
+    setTimeout(function(){ 
+        $(klantenserviceButton).prop('disabled', true);
+    }, 50);
+
+setTimeout(function(){ 
+    $(klantenserviceButton).prop('disabled', false);
+}, 250);
+}
